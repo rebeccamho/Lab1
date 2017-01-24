c3a62bdd8abfd0697358c4b707f47d5da025a6a3
Rebecca Ho <rebeccaho936@gmail.com>
2017-01-24 17:09:32 -0600

    Wrote ST7735_sDecOut3 function and tested it with test in Lab1.c

--- fixed.c
+++ fixed.c
@@ -1,17 +1,38 @@
 #include "stdint.h"
+#include "stdbool.h"
 #include "ST7735.h"
 
 
 void ST7735_sDecOut3(int32_t n) {
-		if(n > 9999 || n < -9999) { // print *.***
-				ST7735_DrawCharS(0, 0, '*', ST7735_Color565(255, 0, 0), 0, 1);
-				ST7735_DrawCharS(0, 6, '.', ST7735_Color565(255, 0, 0), 0, 1);
-				int16_t col = 12;
+		if(n > 9999 || n < -9999) { // number is out of range
+				ST7735_OutChar(' ');
+				ST7735_OutChar('*');
+				ST7735_OutChar('.');
 				for(int i = 0; i < 3; i++) {
-						ST7735_DrawCharS(0, col, '*', ST7735_Color565(255, 0, 0), 0, 1);
-						col = col + 6;
-				}				
+						ST7735_OutChar('*');
+				}
+				return;
+		}
+		bool neg = (n < 0); // number is negative
+		if(neg) { // make n positive so digits are abs value
+				n = -n;
+		}
+		int32_t rem = n % 1000;
+		int32_t thous = n/1000; // first digit
+		int32_t hund = rem/100; // second digit, first after decimal
+		rem = rem % 100;
+		int32_t tens = rem/10; // third digit, sec after decimal
+		int32_t ones = rem % 10; // fourth digit, third after decimal
+		
+		if(neg) {
+			ST7735_OutChar('-');
+		} else {
+			ST7735_OutChar(' ');
 		}
 		
-		//ST7735_DrawCharS(0, 0, 'c', ST7735_Color565(255, 0, 0), 0, 1);
+		ST7735_OutChar((char) thous+48);
+		ST7735_OutChar('.');
+		ST7735_OutChar((char) hund+48);
+		ST7735_OutChar((char) tens+48);
+		ST7735_OutChar((char) ones+48);
 }
